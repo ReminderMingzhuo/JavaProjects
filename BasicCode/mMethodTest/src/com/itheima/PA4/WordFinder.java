9f53e1e760e44b6255e4147ff3c43a0ecdb11e80
mingzhuo <mingzhuo@usc.edu>
2022-04-19 14:22:37 -0700

    'lol'

---
+++ BasicCode/mMethodTest/src/com/itheima/PA4/WordFinder.java
@@ -0,0 +1,26 @@
+//package com.itheima.PA4;
+//
+//import java.util.Scanner;
+//
+//public class WordFinder {
+//    public static void main(String[] args) {
+//        AnagramDictionary anagramDictionary = new AnagramDictionary("sowpods.txt");
+//
+//        System.out.println("Type . to quit.");
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Rack? ");
+//
+//        while (scan.hasNextLine()){
+//            String x = scan.nextLine();
+//            System.out.println(x);
+//            if(x.equals(".")){
+//                break;
+//            } else {
+//                System.out.println("We can make 4 words from \"" + x + "\"");
+//                System.out.println("All of the words with their scores (sorted by score):");
+//            }
+//
+//        }
+//
+//    }
+//}
---
+++ BasicCode/mString/src/com/itheima/string/Demo1String.java
@@ -0,0 +1,10 @@
+package com.itheima.string;
+
+public class Demo1String {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        int length = s1.length();
+        System.out.println(length);
+        s1 = "def"; //这一步是让s1这个字符串类型的变量，记录了一个新的对象
+    }
+}
